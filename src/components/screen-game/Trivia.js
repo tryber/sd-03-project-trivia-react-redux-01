14d084450200897bce4de4e74ca1241872b2cf4f
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-06-09 15:20:47 -0300

    Game-Screen

---
+++ src/components/screen-game/Trivia.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+export default class Game extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+        </div>
+        <div>
+          <p data-testid='question-category'>Categoria</p>
+          <p data-testid='question-text'>Perguntas</p>
+        </div>
+        <div>
+          <button data-testid='correct-answer'>Alternativas</button>
+          <button data-testid='wrong-answer-${index}'>Alternativa incorretas</button>
+        </div>
+        <button data-testid='btn-next'>Proxima</button>
+      </div>
+    )
+  }
+}
