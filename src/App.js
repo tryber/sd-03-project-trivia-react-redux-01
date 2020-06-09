272f498b4e3e8d43e272da3c2767e661ebaa33ff
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-06-09 17:07:09 -0300

    Token

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Login from '../src/components/screen-login/Login';
 import Hanking from './components/sreen-hanking/Hanking';
+import Game from './components/screen-game/Trivia';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={Login} />
       <Route exact path="/hanking" component={Hanking} />
+      <Route exact path="/game" component={Game} />
     </BrowserRouter>
   );
 }
