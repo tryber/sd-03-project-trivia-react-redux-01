937b3f82b960e2f7016001f873662da3c3a32a4d
William Igor <williamigorsilva@gmail.com>
2020-06-09 12:58:58 -0300

    Inicio do projeto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 
 serviceWorker.unregister();
