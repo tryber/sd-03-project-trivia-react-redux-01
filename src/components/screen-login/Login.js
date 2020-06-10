5305f9780347c036f4c489394a76ae3333e78a91
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-06-10 18:17:15 -0300

    Repair codeclimate

--- src/components/screen-login/Login.js
+++ src/components/screen-login/Login.js
@@ -10,6 +10,7 @@ class Login extends Component {
   render() {
     const tokenPlayer = async () => {
       const token = await this.props.requestApiToken();
+      /* const questions = await this.props.requestApiQuestions(); */
       localStorage.setItem('token', token.token);
     };
     return (
