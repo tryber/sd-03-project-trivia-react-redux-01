838acdd2eb179aaedfb131cca5158e9e39b3e4b3
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-06-23 06:05:49 -0300

    cc4

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Proptypes from 'prop-types';
 
 class Header extends Component {
   render() {
-    const { name, score} = this.props;
+    const { name, score } = this.props;
     return (
       <div>
         <header data-testid="header-profile-picture" />
-    <div data-testid="header-player-name">{name}</div>
-    <div data-testid="header-score">{score}</div>
+        <div data-testid="header-player-name">{name}</div>
+        <div data-testid="header-score">{score}</div>
       </div>
     );
   }
 }
 
 const mapStateToProps = () => ({
-score: JSON.parse(localStorage.getItem('state')).player.score || 0,
-name: JSON.parse(localStorage.getItem('state')).player.name || 'Nome da pessoa',
+  score: JSON.parse(localStorage.getItem('state')).player.score || 0,
+  name: JSON.parse(localStorage.getItem('state')).player.name || 'Nome da pessoa',
 });
 
-export default connect(mapStateToProps)(Header);
+Header.propTypes = {
+  name: Proptypes.string,
+  score: Proptypes.number,
+};
 
+export default connect(mapStateToProps)(Header);
